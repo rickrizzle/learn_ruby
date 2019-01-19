bcb72dd91dd3373242ca7164c4ac690e8482b090
rickrizzle <rickh39@gmail.com>
2019-01-19 12:55:45 -0500

    Making Git Work

---
+++ 02_calculator/01_temperature/temperature.rb
@@ -0,0 +1,10 @@
+#write your code here
+def ftoc(f_degrees)
+    c = ((f_degrees - 32) * 5.0 / 9.0).round(1)
+    
+  end
+  
+  def ctof(c_degrees)
+    f = ((c_degrees * 9.0 / 5.0) + 32).round(1)
+    
+  end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,31 @@
 #write your code here
+
+
+def add x,y
+    x+y
+end
+
+def subtract x,y
+    x-y
+end
+
+#computes the sum of an empty array
+def sum(a)
+    a.reduce(0) {|b,c| b + c}
+end
+
+def multiply(a)
+    a.reduce() {|b,c| b * c}
+end
+
+def power(a,b)
+    a ** b
+end
+#need to learn more about this "inject"
+def factorial(a)
+    (1..a).inject(:*)||1
+end
+    
+
+
+
