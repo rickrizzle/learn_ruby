765830444861d1d676a91f2be8f84d45ea900eaa
rickrizzle <rickh39@gmail.com>
2019-01-19 19:19:53 -0500

    "03_simon_says tests"

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
 #write your code here
+
+
+# * functions
+# * strings
+# * default parameter values
+
+
+def echo(word)
+    word
+end
+
+def shout (word)
+    word.upcase
+end
+
+def repeat(word, a = 2)
+    ([word] * a).join(" ")
+end
+
+def start_of_word(word, a =1)
+    word[0...a]
+end
+
+def first_word(word)
+    word.split.first
+end
+
+def titleize(word)
+    words = word.split.map do |word|
+        if %w(the and over).include?(word)
+            word
+        else
+            word.capitalize
+        end
+    end
+    words.first.capitalize!
+    words.join(" ")
+end
+
