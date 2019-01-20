88195c11bcac31577096bf9494e1a11c2f2469db
rickrizzle <rickh39@gmail.com>
2019-01-19 21:20:57 -0500

    Book titles changes

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
+    attr_reader :title
 # write your code here
+    def title=(new_title)
+        words = new_title.split(" ")
+        words = [words[0].capitalized] +
+            words[1..-1].map do |word|
+                little_words = %w{a an and the in of}
+                if little_words.include? word 
+                    word
+                else 
+                    word.capitalize
+                end
+            end
+            @title = words.joint(" ")
+        end
+#
 end
