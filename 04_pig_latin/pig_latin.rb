f1a064f35aefe01a6bd89d9a7cb41f819b532c7c
rickrizzle <rickh39@gmail.com>
2019-01-19 19:59:14 -0500

    Pig_latin tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
 #write your code here
+
+
+def vowels(x)
+    (x =~ /[aeiouAEIOU]/) != nil
+end
+
+def convert(word)
+    while !vowel(word[0])
+        if word[0,2].downcase =='qu'
+            word = word + word[0,2]
+            word = word[2..word.length]
+        else
+            word = word+word[0]
+            word = word[1...word.length]
+        end
+    end
+    word = word + 'ay'
+end
+
+def translate (words)
+    wordArray = words.split
+    wordArray.map! {|w| convert(w)}.join' '
+end
